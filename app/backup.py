582d91acc6d17cd685964ee4b9add09eb49de74b
trizzlenova <tdswayzee@gmail.com>
2023-04-16 16:48:41 -0700

    update pandas to pyspark

--- app/backup.py
+++ app/backup.py
@@ -1,15 +1,13 @@
 from flask import Flask
+from .flows.web_to_local import *
 from pathlib import Path
-from prefect_fugue import fugue_engine
+import os
 
-
-# from .flows import *
-from pyspark.sql import SparkSession, DataFrame
+from pyspark.sql import SparkSession, DataFrame, types
 from prefect import flow, task
-# from schemas import spark, title_crew_schema
-import pyspark
-from pyspark.sql import SparkSession, types
+from prefect_gcp.cloud_storage import GcsBucket
 
+# from schemas import spark, title_crew_schema
 
 title_crew_schema = types.StructType([
     types.StructField('tconst', types.StringType(), True),
@@ -18,58 +16,49 @@ title_crew_schema = types.StructType([
 ])
 
 @task(name = "fetch imdb data", retries = 3)
-def fetch(spark, url: str) -> DataFrame:
+def get_local(path: str) -> DataFrame:
     '''Read data from web into spark dataframe'''
-    print('hit fetch')
-    df = spark.read \
-        .option("header", "true") \
-        .csv(url, sep='\t')
-        # .schema(title_crew_schema) \
-    df.show()
-    return df
 
-
-# @task(log_prints = True)
-# def write_local(df: pd.DataFrame, color: str, dataset_file: str) -> Path:
-#     '''Write dataframe out as a parquet file'''
-#     path = Path(f'data/{dataset_file}.parquet')
-#     df.to_parquet(path, compression = 'gzip')
-#     return path
-
-@flow(log_prints = True)
-def create_app():
-    app = Flask(__name__)
-
+    # path = Path(f'data/title.crew.parquet')
     spark = SparkSession.builder \
         .master("local[*]") \
         .appName('imdb-project') \
         .getOrCreate()
+    
+    df = spark.read \
+        .option("header", "true") \
+        .schema(title_crew_schema) \
+        .parquet(path, sep='\t')
+    
+    df.show()
+    # path = Path(f'app/data/title.crew.parquet')
+    df.write.parquet(
+        path, 
+        compression='gzip',
+        mode = 'overwrite'
+    )
+    
+    return path
 
-    dataset_url = 'https://datasets.imdbws.com/title.crew.tsv.gz'
+@task(log_prints = True)
+def write_gcs(path: Path) -> None:
+    # path = Path(f'data/{dataset_file}.parquet')
+    '''Uploading local parquet file to GCS'''
+    gcp_cloud_storage_bucket_block = GcsBucket.load('prefect-gcs')
+    gcp_cloud_storage_bucket_block.upload_from_path(
+        from_path = f'{path}',
+        to_path = 'data/imdb/title.crew.parquet'
+    )
+    return
 
-    df = fetch(spark, dataset_url)
-    print(f'df: {len(df)}')
-    # df_clean = clean(df)
-    # print(f'df_clean: {len(df_clean)}')
-    # path = write_local(df_clean, color, dataset_file)
+@flow(log_prints = True)
+def create_app():
+    app = Flask(__name__)
+    dirname = os.path.dirname(__file__)
+    file = os.path.join(dirname, 'data/title.crew.parquet')
+    path = get_local(file)
+    print(path)
+    # write_gcs(path)
+
 
     return app
-
-# @flow
-# def native_spark_flow(engine):
-#     with fugue_engine(engine) as engine:
-#         my_spark_task(engine.spark_session, 1)
-
-
-@task
-def my_spark_task(spark, n=1):
-    df = spark.createDataFrame([[f"hello spark {n}"]], "a string")
-    df.show()
-
-@flow
-def native_spark_flow(engine):
-    with fugue_engine(engine) as engine:
-        my_spark_task(engine.spark_session, 1)
-
-spark = SparkSession.builder.getOrCreate()
-native_spark_flow(spark)
--- app/flows/web_to_local.py
+++ app/flows/web_to_local.py
@@ -1,28 +1,56 @@
 from flask import Flask
 from pathlib import Path
-import pandas as pd
 from prefect import flow, task
+import pyspark
+from pyspark.sql import SparkSession, types, DataFrame
+
+
+spark = SparkSession.builder \
+    .master("local[*]") \
+    .appName('test') \
+    .getOrCreate()
+
+title_crew_schema = types.StructType([
+    types.StructField('tconst', types.StringType(), True),
+    types.StructField('directors', types.StringType(), True),
+    types.StructField('writers', types.StringType(), True)
+])
 
 @task(retries = 3)
-def fetch(dataset_url: str) -> pd.DataFrame:
-    '''Read data from web into pandas dataframe'''
-    df = pd.read_csv(dataset_url, sep='\t')
+def fetch(dataset_file: str) -> None:
+    dataset_url = f'https://datasets.imdbws.com/{dataset_file}.tsv.gz'
+    spark.sparkContext.addFile(dataset_url)
+    df = spark.read \
+        .option("header", "true") \
+        .csv(
+            f'file://{pyspark.SparkFiles.get(dataset_file)}.tsv.gz',
+            sep='\t',
+            schema=title_crew_schema
+        )
+    
     return df
 
+
+
 @task(log_prints = True)
-def write_local(df: pd.DataFrame, dataset_file: str) -> Path:
+def write_local(df, dataset_file: str) -> Path:
     '''Write dataframe out as a parquet file'''
-    path = Path(f'data/{dataset_file}.parquet')
-    df.to_parquet(path, compression = 'gzip')
+
+    df.write.parquet(
+        f'app/data/{dataset_file}.parquet', 
+        compression='gzip',
+        mode = 'overwrite'
+    )
+    path = Path(f'app/data/{dataset_file}.parquet')
+
     return path
 
 
-@flow(log_prints = True)
+@task(log_prints = True)
 def web_to_local():
 
     dataset_file = 'title.crew'
-    dataset_url = f'https://datasets.imdbws.com/{dataset_file}.tsv.gz'
 
-    df = fetch(dataset_url)
-    print(f'df: {len(df)}')
+    df = fetch(dataset_file)
     path = write_local(df, dataset_file)
+
