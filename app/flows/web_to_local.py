ec82a7a84a7df92c2b737c57a9743f7b31a552d7
trizzlenova <tdswayzee@gmail.com>
2023-04-16 17:56:04 -0700

    write to gcs

--- app/flows/web_to_local.py
+++ app/flows/web_to_local.py
@@ -1,6 +1,7 @@
 from flask import Flask
-from pathlib import Path
+import glob
 from prefect import flow, task
+from prefect_gcp.cloud_storage import GcsBucket
 import pyspark
 from pyspark.sql import SparkSession, types, DataFrame
 
@@ -33,24 +34,36 @@ def fetch(dataset_file: str) -> None:
 
 
 @task(log_prints = True)
-def write_local(df, dataset_file: str) -> Path:
+def write_local(df, dataset_name: str) -> glob:
     '''Write dataframe out as a parquet file'''
+    path = f'data/imdb/{dataset_name}'
 
     df.write.parquet(
-        f'app/data/{dataset_file}.parquet', 
+        path,
         compression='gzip',
         mode = 'overwrite'
     )
-    path = Path(f'app/data/{dataset_file}.parquet')
 
     return path
 
 
-@task(log_prints = True)
-def web_to_local():
 
+@task(log_prints = True)
+def write_gcs(path: str) -> None:
+    from_file = glob.glob(f'{path}/*.gz.parquet')[0]
+    '''Uploading local parquet file to GCS'''
+    gcp_cloud_storage_bucket_block = GcsBucket.load('prefect-gcs')
+    gcp_cloud_storage_bucket_block.upload_from_path(
+        from_path = from_file,
+        to_path = f'{path}.parquet'
+    )
+    return
+
+
+@flow(log_prints = True)
+def web_to_local():
     dataset_file = 'title.crew'
 
     df = fetch(dataset_file)
     path = write_local(df, dataset_file)
-
+    write_gcs(path)
