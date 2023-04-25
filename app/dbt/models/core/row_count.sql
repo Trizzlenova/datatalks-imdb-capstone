8c6fcad4735dc1e73eb0667f59c961c132a9f574
trizzlenova <tdswayzee@gmail.com>
2023-04-24 22:03:09 -0700

    start core models

---
+++ app/dbt/models/core/row_count.sql
@@ -0,0 +1,22 @@
+with source as (
+    SELECT SUM(tbl.countEach) AS totalRowCount FROM (
+        SELECT COUNT(*) as countEach FROM {{ source('core', 'stg_name_basics') }}
+        UNION ALL
+        SELECT COUNT(*) as countEach FROM {{ source('core', 'stg_title_akas') }}
+        UNION ALL
+        SELECT COUNT(*) as countEach FROM {{ source('core', 'stg_title_basics') }}
+        UNION ALL
+        SELECT COUNT(*) as countEach FROM {{ source('core', 'stg_title_crew') }}
+        UNION ALL
+        SELECT COUNT(*) as countEach FROM {{ source('core', 'stg_title_episode') }}
+        UNION ALL
+        SELECT COUNT(*) as countEach FROM {{ source('core', 'stg_title_principals') }}
+        UNION ALL
+        SELECT COUNT(*) as countEach FROM {{ source('core', 'stg_title_ratings') }}
+    ) tbl
+)
+
+select 
+    *,
+    current_timestamp() as ingestion_timestamp
+from source
